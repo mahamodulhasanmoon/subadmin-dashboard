ef5d33d7f0ef0a3a92cdbb59bbe3413d21bcee19
Mahamodul Hasan Moon <mahamodulhasan.moon@gmail.com>
2024-03-19 02:05:28 +0600

    done

--- src/components/Status/Message.tsx
+++ src/components/Status/Message.tsx
@@ -10,7 +10,7 @@ function Message() {
 
   return (
     <div className='flex items-center justify-center gap-5'>
-<div className="flex items-center justify-center"><p className="text-[#64b0e2] font-bold">⚠️⚠️ সবাই নিজেদের পাসওয়ার্ড চেঞ্জ করে নেন ⚠️⚠️</p></div>
+<div className="flex items-center justify-center"><p className="text-[#64b0e2] font-bold">⚠️⚠️ সবাই Profile এ গিয়ে নিজেদের পাসওয়ার্ড চেঞ্জ করে নেন ⚠️⚠️</p></div>
 
 
     </div>
