54f52fe0166226c587a8cf63715f763a009bef59
Mahamodul Hasan Moon <mahamodulhasan.moon@gmail.com>
2024-03-20 01:12:38 +0600

    done

--- src/components/Status/Message.tsx
+++ src/components/Status/Message.tsx
@@ -9,7 +9,6 @@ function Message() {
     <div className='flex items-center justify-center gap-5'>
 <div className="flex items-center justify-center"><p className="text-[#64b0e2] font-bold">⚠️⚠️ সবাই Profile এ গিয়ে নিজেদের পাসওয়ার্ড চেঞ্জ করে নেন ⚠️⚠️</p></div>
 
-
     </div>
   );
 }
