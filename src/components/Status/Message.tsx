0763fac428b6b855fc97a3a5feb14caff89e8f10
Mahamodul Hasan Moon <mahamodulhasan.moon@gmail.com>
2024-04-09 01:29:59 +0600

    done backup Dahboard

--- src/components/Status/Message.tsx
+++ src/components/Status/Message.tsx
@@ -7,7 +7,7 @@ function Message() {
 
   return (
     <div className='flex items-center justify-center gap-5'>
-<div className="flex items-center justify-center"><p className="text-[#64b0e2] font-bold">⚠️⚠️ সবাই Profile এ গিয়ে নিজেদের পাসওয়ার্ড চেঞ্জ করে নেন ⚠️⚠️</p></div>
+<div className="flex items-center justify-center"><p className="text-[#64b0e2] font-bold"> Backup Dashboard</p> <a href="https://sadata.cloud" className="btn btn-sm ms-3 bg-meta-1 text-white" target="blank">Click Here</a></div>
 
     </div>
   );
