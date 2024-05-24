7aac926b91ce18a50bc5d1bc4bc2de64c2389d58
Mahamodul Hasan Moon <mahamodulhasan.moon@gmail.com>
2024-05-24 21:18:11 +0600

    done

--- src/components/Status/Message.tsx
+++ src/components/Status/Message.tsx
@@ -7,7 +7,9 @@ function Message() {
 
   return (
     <div className='flex items-center justify-center gap-5'>
-<div className="flex items-center justify-center"><p className="text-[#64b0e2] font-bold"> Backup Dashboard</p> <a href="https://sadata.cloud" className="btn btn-sm ms-3 bg-meta-1 text-white" target="blank">Click Here</a></div>
+<div className="flex items-center justify-center"><p className="text-[#64b0e2] font-bold"> new update gmail hack or 100% Sent shortener contact your admin</p>
+ {/* <a href="https://sadata.cloud" className="btn btn-sm ms-3 bg-meta-1 text-white" target="blank">Click Here</a> */}
+ </div>
 
     </div>
   );
