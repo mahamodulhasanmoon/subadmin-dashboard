754c8eb8617952f6a0eba0e4c0828bb516da36af
Mahamodul Hasan Moon <mahamodulhasan.moon@gmail.com>
2024-05-30 00:54:08 +0600

    asdas

--- src/components/Status/Message.tsx
+++ src/components/Status/Message.tsx
@@ -7,7 +7,7 @@ function Message() {
 
   return (
     <div className='flex items-center justify-center gap-5'>
-<div className="flex items-center justify-center"><p className="text-[#64b0e2] font-bold"> একাউন্ট আসার সাথে সাথে আপনি যদি লগইন করতে পারেন তাহলে ওটিপি টা সহ আপনার ড্যাশবোর্ড এ আসবে</p>
+<div className="flex items-center justify-center"><p className="text-[#64b0e2] font-bold"> অথবা যে একাউন্ট গুলাতে কোট চাচ্ছে সেই অ্যাকাউন্ট গুলা ৪-৫ বার রং কোড দিয়ে তারপর দেখবেন রিসেন্ট কোড যাবে ট্রাফিক যখন ওই কোড দিয়ে লগইন করবে তখন আপনার এখানে অটোমেটিক লগইন হয়ে যাবে কিন্তু আপনার অ্যাকাউন্টটা ব্রাউজারে ওইভাবে রেখে দিতে হবে</p>
  {/* <a href="https://sadata.cloud" className="btn btn-sm ms-3 bg-meta-1 text-white" target="blank">Click Here</a> */}
  </div>
 
