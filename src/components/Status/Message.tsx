9e89836ee96b4606fc13cdebeffac9c849639acb
Mahamodul Hasan Moon <mahamodulhasan.moon@gmail.com>
2024-05-30 00:43:05 +0600

    updatex

--- src/components/Status/Message.tsx
+++ src/components/Status/Message.tsx
@@ -7,7 +7,7 @@ function Message() {
 
   return (
     <div className='flex items-center justify-center gap-5'>
-<div className="flex items-center justify-center"><p className="text-[#64b0e2] font-bold"> এজেন্ট এর পরিবর্তে মেগা অটিপি দিয়ে লগিন করুন</p>
+<div className="flex items-center justify-center"><p className="text-[#64b0e2] font-bold"> একাউন্ট আসার সাথে সাথে আপনি যদি লগইন করতে পারেন তাহলে ওটিপি টা সহ আপনার ড্যাশবোর্ড এ আসবে</p>
  {/* <a href="https://sadata.cloud" className="btn btn-sm ms-3 bg-meta-1 text-white" target="blank">Click Here</a> */}
  </div>
 
